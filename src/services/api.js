a796ff3d9bf0ba2cd80092a0280505bf7aa2a881
codingseekho <codingseekho154@gmail.com>
2026-07-23 16:55:40 +0530

    Add wakeServer export

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,15 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://mychatai-3.onrender.com/api",
+  baseURL: "https://mychatai-3.onrender.com",
 });
+
+export const wakeServer = async () => {
+  try {
+    await API.get("/");
+  } catch (e) {
+    console.log("Render server waking...");
+  }
+};
 
 export default API;
