854dc39788c191444f71da8901d3edb0ea0ed4f6
codingseekho <codingseekho154@gmail.com>
2026-07-23 20:17:08 +0530

    Restore App Login Chat API

--- src/services/api.js
+++ src/services/api.js
@@ -1,15 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://mychatai-3.onrender.com",
+  baseURL: "https://mychatai-3.onrender.com/api",
 });
-
-export const wakeServer = async () => {
-  try {
-    await API.get("/");
-  } catch (e) {
-    console.log("Render server waking...");
-  }
-};
 
 export default API;
