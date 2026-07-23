54afb6630da407b260d6572cf298d84c9534ffb2
codingseekho <codingseekho154@gmail.com>
2026-07-23 16:37:28 +0530

    Update API URL

--- src/services/api.js
+++ src/services/api.js
@@ -1,16 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://mychatai-3.onrender.com",
-  timeout: 60000
+  baseURL: "https://mychatai-3.onrender.com/api",
 });
-
-export const wakeServer = async () => {
-  try {
-    await fetch("https://mychatai-3.onrender.com/");
-  } catch (e) {
-    console.log("Waking Render server...");
-  }
-};
 
 export default API;
